a3c73e7aedfc8c07a59de93e1f7290f6ed5d1ce3
nonnycode <miknonny@gmail.com>
2016-06-25 17:05:18 +0100

    added limiter for network request

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const readline = require('linebyline')
 const process = require('process')
 const colors = require('colors')
 const fs = require('fs')
-
+const RateLimiter = require('limiter').RateLimiter
+const limiter = new RateLimiter(1, 250)
 
 if (process.argv.length < 3 ) {
   console.log('Usage: mx-resolve <searchTerm> <sourceFile> <destFile>')
@@ -20,22 +21,22 @@ const destFile = process.argv[4]
 
 
 rl.on('line', (line, linecount) => {
-  dns.resolveMx(line.split('@')[1])
-  .then(address => {
-    // console.log(address[1].exchange)
-    // console.log(line, address[1].exchange.includes(domain))
-    if (address[1].exchange.includes(domain)) {
-      console.log(line.green, address[1].exchange.green)
-      fs.appendFile(destFile, line + '\n', function (err) {
-        if (err) throw err;
-      })
-    } else {
-      console.log(line.red, address[1].exchange)
-    }
+  limiter.removeTokens(1, () => {
+    dns.resolveMx(line.split('@')[1])
+    .then(address => {
+      // console.log(address[1].exchange)
+      // console.log(line, address[1].exchange.includes(domain))
+      if (address[1].exchange.includes(domain)) {
+        console.log(line.green, address[1].exchange.green)
+        fs.appendFile(destFile, line + '\n', function (err) {
+          if (err) throw err;
+        })
+      } else {
+        console.log(line.red, address[1].exchange)
+      }
+    })
   })
-  console.log(linecount)
 })
 .on('error', e => {
   console.log(e)
 })
-console.log('done!')
